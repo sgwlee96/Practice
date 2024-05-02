890e53dc5819161d7e35a742bf0f7f0f1954b258
sgwlee96 <sgwlee@ucdavis.edu>
2024-05-02 02:15:57 -0700

    change batch size

--- 1_CNN/main.py
+++ 1_CNN/main.py
@@ -8,7 +8,7 @@ def main():
 
     train_img, train_label, validate_img, validate_label, test_img, test_label = model.load_data()
     model_instance = model.build_model()
-    history = model_instance.fit(train_img, train_label, epochs=10, batch_size=2, validation_data=(validate_img, validate_label))
+    history = model_instance.fit(train_img, train_label, epochs=10, batch_size=16, validation_data=(validate_img, validate_label))
     test_loss, test_accuracy = model_instance.evaluate(test_img, test_label)
 
     utils.save_results(history, test_loss, test_accuracy, csv_file, append=file_exists)
