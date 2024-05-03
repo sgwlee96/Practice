f057fd83201934f1f1d617ad0437342e793461d1
sgwlee96 <sgwlee@ucdavis.edu>
2024-05-02 17:42:14 -0700

    result update

--- 1_CNN/main.py
+++ 1_CNN/main.py
@@ -6,12 +6,14 @@ def main():
     csv_file = "results.csv"
     file_exists = os.path.isfile(csv_file)
 
+
+    model_name = "10Layers_CNN"
     train_img, train_label, validate_img, validate_label, test_img, test_label = model.load_data()
     model_instance = model.build_model()
     history = model_instance.fit(train_img, train_label, epochs=10, batch_size=16, validation_data=(validate_img, validate_label))
     test_loss, test_accuracy = model_instance.evaluate(test_img, test_label)
 
-    utils.save_results(history, test_loss, test_accuracy, csv_file, append=file_exists)
+    utils.save_results(model_name, history, test_loss, test_accuracy, csv_file, append=file_exists)
 
 
 if __name__ == "__main__":
--- 1_CNN/utils.py
+++ 1_CNN/utils.py
@@ -1,15 +1,16 @@
 import csv
 
-def save_results(history, test_loss, test_accuracy, csv_file, append=False):
+def save_results(model_name, history, test_loss, test_accuracy, csv_file, append=False):
     csv_file = "results.csv"
     with open(csv_file, mode="a" if append else 'w', newline='') as file:
         writer = csv.writer(file)
 
         if not append:
 
-            writer.writerow(["Train Loss", "Train Accuracy", "Validation Loss", "Validation Accuracy", "Test Loss", "Test Accuracy"])
+            writer.writerow(["Model","Train Loss", "Train Accuracy", "Validation Loss", "Validation Accuracy", "Test Loss", "Test Accuracy"])
 
-        writer.writerow([history.history['loss'][-1],
+        writer.writerow([model_name,
+                         history.history['loss'][-1],
                          history.history['accuracy'][-1],
                          history.history["val_loss"][-1], 
                          history.history["val_accuracy"][-1], 
